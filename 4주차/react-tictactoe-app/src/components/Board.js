9e6e53818f37e0457f636a04bc126a082d818ca3
xudegloss <ajouunihospitalrn@naver.com>
2023-05-28 22:19:02 +0900

    리액트 실습하기

---
+++ 4주차/react-tictactoe-app/src/components/Board.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Square from "./Square";
+import "./Board.css";
+
+export default class Board extends Component {
+  // renderSquare 함수 : Square Component 가져오기.
+  // 반복성이 높아서, Square 자체를 Component로 제작한 것이다.
+  renderSquare() {
+    return <Square />;
+  }
+
+  // this는 class Board를 가르킨다.
+  render() {
+    const status = "New player: X, O";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ 4주차/react-tictactoe-app/src/components/Square.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Square.css";
+
+export default class Square extends Component {
+  render() {
+    return <button className="square"></button>;
+  }
+}
