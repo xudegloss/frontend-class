540424bcb522c4c39d24e159120f4b356bb439a1
xudegloss <ajouunihospitalrn@naver.com>
2023-06-06 23:43:39 +0900

    하나만 X 되는 부분 오류 해결하기

--- 4주차/react-tictactoe-app-review/src/Components/Square.js
+++ 4주차/react-tictactoe-app-review/src/Components/Square.js
@@ -4,9 +4,7 @@ import "./Square.css";
 export default class Square extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: [...Array(9).keys()],
-    };
+    this.state = { squares: [...Array(9).keys()] };
   }
 
   render() {
@@ -16,7 +14,12 @@ export default class Square extends Component {
       <button
         className="square"
         onClick={() => {
-          this.setState({ squares: "X" });
+          // 각각의 인덱스에 따라서 X로 변경해야 한다.
+          // this.setState({squares[idx] : "X"}) 는 오류가 뜬다. 왜 뜨는지는 모르겠다.
+          // 그래서 새로운 Square를 만들고 변경 후 그 값으로 변경해버린다.
+          const newSquares = this.state.squares;
+          newSquares[idx] = "X";
+          this.setState({ newSquares });
         }}
       >
         {this.state.squares[idx]}
